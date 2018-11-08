5ed4e0422bbc8ee6cbf00ea4dc388e10ffaefa24
lizardqueen98 <nadija.b@gmail.com>
2018-11-08 11:48:10 +0100

    hejt

---
+++ src/Semestar.java
@@ -0,0 +1,21 @@
+public class Semestar {
+    private int broj_semestra;
+    private Predmet[] predmeti;
+    private int broj_predmeta;
+    public Semestar(int broj_semestra){
+        broj_predmeta=0;
+    }
+    public void dodajPredmet(Predmet p){
+
+    }
+    public boolean daLiJeNaSemestru(Predmet p){
+        return true;
+    }
+    public void ispisiPredmet(Predmet p){
+
+    }
+
+    public int getBroj_semestra() {
+        return broj_semestra;
+    }
+}
