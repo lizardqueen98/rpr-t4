68155db1160c8df519c905954d33ca15f69c729f
student <emi.cazorla@hotmail.com>
2018-11-08 14:25:46 +0100

    izmjene

--- src/ba/unsa/etf/rpr/tutorijal4/Fakultet.java
+++ src/ba/unsa/etf/rpr/tutorijal4/Fakultet.java
@@ -1,10 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal4;
 
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Fakultet {
-    private TreeSet<Student> s;
-    private TreeSet<Predmet> p;
-    private TreeSet<Upis> u;
-
+    private Set<Student> studenti;
+    private Set<Predmet> predmeti;
+    private Set<Upis> upisani;
+    public Fakultet(){
+        studenti=new TreeSet<>();
+        predmeti=new TreeSet<>();
+        upisani=new TreeSet<>();
+    }
+    public void dodajStudenta(Student s){
+        studenti.add(s);
+    }
+    public void dodajPredmet(Predmet p){
+        predmeti.add(p);
+    }
+    public void upisi(Student s, Predmet p, PlanStudija ps){
+        upisani.add(new Upis(s,p,ps));
+    }
 }
