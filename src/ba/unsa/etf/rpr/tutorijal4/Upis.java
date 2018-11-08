68c335ca5e21bdb5f81fb0dd7fdaf8aa39fa7f55
student <emi.cazorla@hotmail.com>
2018-11-08 14:15:10 +0100

    izmjene

--- src/ba/unsa/etf/rpr/tutorijal4/Upis.java
+++ src/ba/unsa/etf/rpr/tutorijal4/Upis.java
@@ -4,4 +4,33 @@ public class Upis {
     private Student s;
     private Predmet p;
     private PlanStudija ps;
+    public Upis(Student s, Predmet p, PlanStudija ps){
+        this.s=s;
+        this.p=p;
+        this.ps=ps;
+    }
+
+    public void setP(Predmet p) {
+        this.p = p;
+    }
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Predmet getP() {
+        return p;
+    }
+
+    public PlanStudija getPs() {
+        return ps;
+    }
+
+    public void setPs(PlanStudija ps) {
+        this.ps = ps;
+    }
 }
