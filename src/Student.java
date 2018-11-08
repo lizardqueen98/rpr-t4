2f0307f3b704f01094eb4de3c931ad0b9edd138a
lizardqueen98 <nadija.b@gmail.com>
2018-11-08 11:35:23 +0100

    hejt

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    private String ime,prezime;
+    private int broj_indeksa;
+    private int[] predmeti_sifra;
+    public Student(String ime, String prezime, int broj_indeksa){
+
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public int getBroj_indeksa() {
+        return broj_indeksa;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public void setBroj_indeksa(int broj_indeksa) {
+        this.broj_indeksa = broj_indeksa;
+    }
+}
